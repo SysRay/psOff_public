537d1acb36c060c5fdda1b0fd0569bc8d13ac6cd
Martin Baliet <s.baliet@gmx.de>
2024-02-27 23:34:08 +0100

    prjSplit| include third party, fix CMakeLists

--- modules/setupModule.cmake
+++ modules/setupModule.cmake
@@ -1,6 +1,6 @@
 function(setupModule _Target)
   add_custom_command(TARGET ${_Target} POST_BUILD
-    COMMAND ${EMULATOR_ROOT}/bin/dll2Nids.exe ${CMAKE_CURRENT_BINARY_DIR}/${_Target}.dll
+    COMMAND ${EMULATOR_DEV_ROOT}/bin/dll2Nids.exe ${CMAKE_CURRENT_BINARY_DIR}/${_Target}.dll
   )
 
   install(FILES $<TARGET_PDB_FILE:${_Target}> DESTINATION debug OPTIONAL)
