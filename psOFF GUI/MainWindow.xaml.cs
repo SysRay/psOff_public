4e9554777b0fc761c805102bc6ec1f60bc133f52
ChristianStrau <98279829+ChristianStrau@users.noreply.github.com>
2024-03-16 11:01:24 +0100

    Init

---
+++ psOFF GUI/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace psOFF_GUI
+{
+    public partial class MainWindow : Window
+    {
+
+   
+        
+        
+
+        public MainWindow()
+        {
+            string currentDir = System.IO.Directory.GetCurrentDirectory();
+            string currentDirExe = currentDir + "/emulator.exe";
+
+
+            InitializeComponent();
+          
+        }
+
+
+
+
+        public void MainGrid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Check if there is at least one file and if it has the ".bin" extension.
+                if (files.Length > 0 && (IsFileExtensionValid(files[0], ".bin") || IsFileExtensionValid(files[0], ".elf")))
+                {
+                    Pfad.Text = files[0];
+                    OpenWindow1(files[0]);
+                }
+                else
+                {
+                    MessageBox.Show("Please drop a valid .bin or .elf file.", "Invalid File Type");
+                }
+            }
+        }
+
+        private bool IsFileExtensionValid(string filePath, string extension)
+        {
+            return System.IO.Path.GetExtension(filePath).Equals(extension, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public void OpenWindow1(string filePath)
+        {
+            Window1 window1 = new Window1(filePath);
+            window1.Show();
+
+            // Close the current window if needed
+            this.Close();
+        }
+
+       
+    }
+}
---
+++ psOFF GUI/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace psOFF_GUI
+{
+    public partial class Window1 : Window
+    {
+        private string filePath;
+        
+        public Window1(string filePath)
+        {
+            InitializeComponent();
+            // Set the filePath property
+            this.filePath = filePath;
+
+            // Call the method to set the image source
+            SetImageSource();
+        }
+
+        private void SetImageSource()
+        {
+            testText.Text = "0";
+            // Check if filePath is not empty
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                testText.Text = "1";
+                try
+                {
+                    // Construct the path to the image dynamically based on the provided file path
+                    string directoryPath = System.IO.Path.GetDirectoryName(filePath);
+                    string imagePath = System.IO.Path.Combine(directoryPath, "sce_sys", "icon0.png");
+                    testText.Text = imagePath; 
+                    // Set the source of the Image
+                    if (File.Exists(imagePath))
+                    {
+                        iconImage.Source = new BitmapImage(new Uri(imagePath));
+                    }
+                    else
+                    {
+                        // Handle case where image file does not exist
+                        MessageBox.Show("Image file not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    testText.Text = "2";
+                    // Handle any exceptions
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
