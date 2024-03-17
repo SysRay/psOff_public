6c3e2316151d240b9b7c894d8a45cbf44fa11ccd
ChristianStrau <christianstrauss6@gmail.com>
2024-03-17 11:05:48 +0100

    Setup

---
+++ gui/psOFF GUI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace psOFF_GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+     
+    }
+
+}
+
+
