a335a525b9fe849c113279efdf461bbe65b8c116
igor725 <gvaldovigor@gmail.com>
2024-03-18 20:13:46 +0300

    Move ITypeRandom to type_random.cpp

--- core/fileManager/types/type_random.cpp
+++ core/fileManager/types/type_random.cpp
@@ -1,11 +1,26 @@
 #include "type_random.h"
 
-std::unique_ptr<TypeRandom> createType_random() {
+class ITypeRandom: public IFile {
+  public:
+  ITypeRandom(): IFile(FileType::Device) {}
+
+  virtual ~ITypeRandom() {}
+
+  // ### Interface
+  size_t  read(void* buf, size_t nbytes) final;
+  size_t  write(void* buf, size_t nbytes) final;
+  int64_t lseek(int64_t offset, SceWhence whence) final;
+  void    sync() final;
+  bool    isError() final;
+  void    clearError() final;
+};
+
+std::unique_ptr<IFile> createType_random() {
   std::srand(std::time(nullptr));
-  return std::make_unique<TypeRandom>();
+  return std::make_unique<ITypeRandom>();
 }
 
-size_t TypeRandom::read(void* buf, size_t nbytes) {
+size_t ITypeRandom::read(void* buf, size_t nbytes) {
   auto cbuf = static_cast<char*>(buf);
 
   for (size_t i = 0; i < nbytes; i++)
@@ -14,18 +29,18 @@ size_t TypeRandom::read(void* buf, size_t nbytes) {
   return nbytes;
 }
 
-size_t TypeRandom::write(void* buf, size_t nbytes) {
+size_t ITypeRandom::write(void* buf, size_t nbytes) {
   return 0;
 }
 
-int64_t TypeRandom::lseek(int64_t offset, SceWhence whence) {
+int64_t ITypeRandom::lseek(int64_t offset, SceWhence whence) {
   return -1;
 }
 
-void TypeRandom::sync() {}
+void ITypeRandom::sync() {}
 
-bool TypeRandom::isError() {
+bool ITypeRandom::isError() {
   return false;
 }
 
-void TypeRandom::clearError() {}
+void ITypeRandom::clearError() {}
--- core/fileManager/types/type_random.h
+++ core/fileManager/types/type_random.h
@@ -4,19 +4,4 @@
 
 #include <memory>
 
-class TypeRandom: public IFile {
-  public:
-  TypeRandom(): IFile(FileType::Device) {}
-
-  virtual ~TypeRandom() {}
-
-  // ### Interface
-  size_t  read(void* buf, size_t nbytes) final;
-  size_t  write(void* buf, size_t nbytes) final;
-  int64_t lseek(int64_t offset, SceWhence whence) final;
-  void    sync() final;
-  bool    isError() final;
-  void    clearError() final;
-};
-
-std::unique_ptr<TypeRandom> createType_random();
+std::unique_ptr<IFile> createType_random();
