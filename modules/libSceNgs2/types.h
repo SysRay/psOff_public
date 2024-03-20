a5c9bd7d431a73f38f4770f9b4247e151e792d52
igor725 <gvaldovigor@gmail.com>
2024-03-21 00:28:58 +0300

    Format again

--- modules/libSceNgs2/types.h
+++ modules/libSceNgs2/types.h
@@ -3,4 +3,4 @@
 #include "codes.h"
 
 typedef struct SceWaveformFormat SceWaveformFormat;
-typedef int                      (*SceWaveformUserFunc)(size_t, uint32_t, void*, size_t);
+typedef int (*SceWaveformUserFunc)(size_t, uint32_t, void*, size_t);
