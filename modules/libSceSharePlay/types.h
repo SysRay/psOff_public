c7dfbeb6a09ee7e5d55f22ef91f599dec22e9063
Tyler Jaacks <TylerJaacks@Gmail.com>
2024-03-30 16:53:05 -0500

    Updated unsigned int to uint32_t

--- modules/libSceSharePlay/types.h
+++ modules/libSceSharePlay/types.h
@@ -2,19 +2,19 @@
 #include "codes.h"
 #include "modules/libSceNpManager/types.h"
 
-enum class SceSharePlayProhibitionMode : unsigned int {
+enum class SceSharePlayProhibitionMode : uint32_t {
   SCE_SHARE_PLAY_PROHIBITION_MODE_OFF = 0,
   SCE_SHARE_PLAY_PROHIBITION_MODE_CONTROL_ONLY = 1,
   SCE_SHARE_PLAY_PROHIBITION_MODE_CONTROL_SCREEN = 2
 };
 
-enum class SceSharePlayConnectionStatus : unsigned int {
+enum class SceSharePlayConnectionStatus : uint32_t {
   SCE_SHARE_PLAY_CONNECTION_STATUS_DORMANT = 0,
   SCE_SHARE_PLAY_CONNECTION_STATUS_READY = 1,
   SCE_SHARE_PLAY_CONNECTION_STATUS_CONNECTED = 2,
 };
 
-enum class SceSharePlayControllerMode : unsigned int {
+enum class SceSharePlayControllerMode : uint32_t {
   SCE_SHARE_PLAY_CONTROLLER_MODE_WATCHING_HOST_PLAY = 0,
   SCE_SHARE_PLAY_CONTROLLER_MODE_PLAYING_AS_HOST = 1,
   SCE_SHARE_PLAY_CONTROLLER_MODE_PLAYING_GAME_WITH_HOST = 2,
