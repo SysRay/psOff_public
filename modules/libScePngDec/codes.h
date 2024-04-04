ad40eda024556f361aa33388be90409fc1d20ce4
igor725 <gvaldovigor@gmail.com>
2024-04-05 01:23:12 +0300

    No hex there

--- modules/libScePngDec/codes.h
+++ modules/libScePngDec/codes.h
@@ -2,14 +2,14 @@
 #include <stdint.h>
 
 namespace Err {
-constexpr int32_t INVALID_ADDR   = 0x80690001;
-constexpr int32_t INVALID_SIZE   = 0x80690002;
-constexpr int32_t INVALID_PARAM  = 0x80690003;
-constexpr int32_t INVALID_HANDLE = 0x80690004;
-constexpr int32_t INVALID_DATA   = 0x80690010;
-constexpr int32_t UNSUPPORT_DATA = 0x80690011;
-constexpr int32_t DECODE_ERROR   = 0x80690012;
-constexpr int32_t FATAL          = 0x80690020;
+constexpr int32_t INVALID_ADDR   = -2140602367;
+constexpr int32_t INVALID_SIZE   = -2140602366;
+constexpr int32_t INVALID_PARAM  = -2140602365;
+constexpr int32_t INVALID_HANDLE = -2140602364;
+constexpr int32_t INVALID_DATA   = -2140602352;
+constexpr int32_t UNSUPPORT_DATA = -2140602351;
+constexpr int32_t DECODE_ERROR   = -2140602350;
+constexpr int32_t FATAL          = -2140602336;
 } // namespace Err
 
 enum class ScePngDecColorSpace : uint16_t {
