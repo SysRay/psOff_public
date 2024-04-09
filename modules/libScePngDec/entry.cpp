d06488f4dee9390218c9d9703bb8a852ebfc2c94
igor725 <gvaldovigor@gmail.com>
2024-04-09 19:58:25 +0300

    Fix formatting

--- modules/libScePngDec/entry.cpp
+++ modules/libScePngDec/entry.cpp
@@ -167,7 +167,7 @@ EXPORT SYSV_ABI int32_t scePngDecParseHeader(const ScePngDecParseParam* param, S
   png_get_IHDR(png_ptr, info_ptr, &ii->imageWidth, &ii->imageHeight, &bitdepth, &color, &inter, nullptr, &filter);
 
   ii->imageFlag  = 0;
-  ii->bitDepth = bitdepth;
+  ii->bitDepth   = bitdepth;
   ii->colorSpace = map_png_color(color);
   if (inter == 1) ii->imageFlag |= SCE_PNG_DEC_IMAGE_FLAG_ADAM7_INTERLACE;
   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) ii->imageFlag |= SCE_PNG_DEC_IMAGE_FLAG_TRNS_CHUNK_EXIST;
