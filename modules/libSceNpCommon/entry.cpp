0f19c6d7961fdda6506e44ca51e400d17580129e
krispy <125815038+kr1spy2115@users.noreply.github.com>
2024-04-14 16:28:14 +0200

    format document

--- modules/libSceNpCommon/entry.cpp
+++ modules/libSceNpCommon/entry.cpp
@@ -7,7 +7,6 @@ LOG_DEFINE_MODULE(libSceNpCommon);
 
 namespace {} // namespace
 
-
 extern "C" {
 
 EXPORT const char* MODULE_NAME = "libSceNpCommon";
@@ -17,8 +16,8 @@ EXPORT SYSV_ABI void __DUMMY(FUNCTION_1)() {}
 EXPORT SYSV_ABI int32_t sceNpCmpNpId() {
   return Ok;
 }
+
 EXPORT SYSV_ABI int32_t sceNpMutexInit() {
   return Ok;
 }
 }
-
