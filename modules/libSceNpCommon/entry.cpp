fc413c7da28cbd76ab5863a4979196d18616a548
krispy <125815038+kr1spy2115@users.noreply.github.com>
2024-04-14 15:48:18 +0200

    formatting fix

--- modules/libSceNpCommon/entry.cpp
+++ modules/libSceNpCommon/entry.cpp
@@ -17,7 +17,7 @@ EXPORT SYSV_ABI void __DUMMY(FUNCTION_1)() {}
 EXPORT SYSV_ABI int32_t sceNpCmpNpId() {
   return Ok;
 }
-  EXPORT SYSV_ABI int32_t sceNpMutexInit() {
+EXPORT SYSV_ABI int32_t sceNpMutexInit() {
   return Ok;
 }
 }
