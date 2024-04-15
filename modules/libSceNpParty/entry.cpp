61010a8234b2563223c16d221312cf6b607a3f38
igor725 <gvaldovigor@gmail.com>
2024-04-16 01:17:53 +0300

    Some stubs

--- modules/libSceNpParty/entry.cpp
+++ modules/libSceNpParty/entry.cpp
@@ -20,4 +20,70 @@ EXPORT SYSV_ABI int32_t sceNpPartyCheckCallback() {
   LOG_ERR(L"todo %S", __FUNCTION__);
   return Ok;
 }
+
+EXPORT SYSV_ABI int32_t sceNpPartyCreate() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetId() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetMemberInfo() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetMemberVoiceInfo() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetMembers() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetState() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyGetStateAsUser() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyRegisterHandler() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartySendBinaryMessage() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyShowInvitationList() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
+
+EXPORT SYSV_ABI int32_t sceNpPartyTerminate() {
+  LOG_USE_MODULE(libSceNpParty);
+  LOG_ERR(L"todo %s", __FUNCTION__);
+  return Ok;
+}
 }
