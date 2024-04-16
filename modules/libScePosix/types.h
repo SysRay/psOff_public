a97aebe23f6e16c0e629906acb08b0a24c983308
Ordinary205 <125112932+Ordinary205@users.noreply.github.com>
2024-04-16 23:43:27 +0400

    +

--- modules/libScePosix/types.h
+++ modules/libScePosix/types.h
@@ -2,3 +2,13 @@
 #include "codes.h"
 
 struct SceNetId{};
+
+struct SceNetSaFamily_t{};
+
+struct SceNetSockaddr{
+  uint8_t sa_len;
+	SceNetSaFamily_t sa_family;
+	char sa_data[14];
+};
+
+struct SceNetSocklen_t{};
