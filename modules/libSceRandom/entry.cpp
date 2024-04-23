8ed37c12b0eeb771a3c87d55c2f365ca056519d9
Ordinary205 <125112932+Ordinary205@users.noreply.github.com>
2024-04-23 20:21:07 +0400

    +

--- modules/libSceRandom/entry.cpp
+++ modules/libSceRandom/entry.cpp
@@ -12,8 +12,12 @@ EXPORT const char* MODULE_NAME = "libSceRandom";
 
 EXPORT SYSV_ABI int32_t sceRandomGetRandomNumber(void* buf, uint32_t size) {
   LOG_USE_MODULE(libSceRandom);
-  if (buf == nullptr || size > Err::RANDOM_MAX_SIZE) {
+  if (size > Err::RANDOM_MAX_SIZE) {
     return EINVAL;
+      }
+  for (uint32_t i = 0; i < size; ++i) {
+    ((uint8_t*)buf)[i] = std::rand() & 0xFF;
   }
+  return 0;
 }
 }
