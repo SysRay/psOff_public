b390e6ea5d69d44e2fed0c8510a95173456a865d
igor725 <gvaldovigor@gmail.com>
2024-04-30 15:57:04 +0300

    Update codes.h

--- modules/libSceShareUtility/codes.h
+++ modules/libSceShareUtility/codes.h
@@ -8,7 +8,7 @@ constexpr int32_t HEAP_SIZE          = 131072;
 constexpr int32_t UPLOAD_CONTENT_MAX = 8192;
 } // namespace ShareUtility
 
-enum SceContentSearchMimeType {
+enum class SceContentSearchMimeType {
   UNKNOWN = 0,
   JPEG    = 1,
   MP4     = 2,
