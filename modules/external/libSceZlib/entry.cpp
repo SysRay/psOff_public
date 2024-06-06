62da9849bf5c65df2450d721195273a818b6b3a8
igor725 <gvaldovigor@gmail.com>
2024-06-06 11:45:39 +0300

    Actual decompression code

--- modules/external/libSceZlib/entry.cpp
+++ modules/external/libSceZlib/entry.cpp
@@ -19,14 +19,15 @@ public:
       PROCESSING,
       DONE,
       ERROR,
+      READY,
     };
 
     Item(const void* src, const uint32_t srcLen, void* dst, uint32_t dstLen): m_src(src), m_srcLen(srcLen), m_dst(dst), m_dstLen(dstLen) {};
     virtual ~Item() = default;
 
-    inline bool isFinished() { return m_status == Status::DONE || m_status == Status::ERROR; }
+    inline bool isFinished() { return m_zret == Z_OK && m_status == Status::READY; }
 
-    inline bool isErrored() { return m_status == Status::ERROR; }
+    inline bool isErrored() { return m_zret != Z_OK; }
 
     inline Status getStatus() const { return m_status; }
 
@@ -38,13 +39,36 @@ public:
         return;
       }
 
+      m_zstrm.next_in  = (Bytef*)m_src;
+      m_zstrm.avail_in = m_srcLen;
+
+      m_zstrm.next_out  = (Bytef*)m_dst;
+      m_zstrm.avail_out = m_dstFinalLen;
+
       m_status = Status::ERROR;
     }
 
     void decompressionStep() {
-      LOG_USE_MODULE(libSceZlib);
-      LOG_ERR(L"Unimplemented");
-      m_status = Status::ERROR;
+      if ((m_zret = inflate(&m_zstrm, Z_NO_FLUSH)) != Z_OK) {
+        switch (m_zret) {
+          case Z_NEED_DICT:
+          case Z_DATA_ERROR:
+          case Z_MEM_ERROR: {
+            m_status = Status::ERROR;
+            return;
+          }
+
+          // There are some non-fatal errors we can safely ignore
+          default: {
+            m_zret = Z_OK;
+          }
+        }
+      }
+
+      if (m_zstrm.avail_in == 0) {
+        m_status      = Status::DONE;
+        m_dstFinalLen = m_zstrm.total_out;
+      }
     }
 
     void finishDecompression() {
@@ -52,6 +76,11 @@ public:
         inflateEnd(&m_zstrm);
         m_bClosed = true;
       }
+    }
+
+    void ready() {
+      finishDecompression();
+      m_status = Status::READY;
     }
 
     int32_t zlibErrorToSce() {
@@ -65,15 +94,15 @@ private:
     Status m_status  = Status::IDLE;
     bool   m_bClosed = false;
 
-    const void*    m_src;
-    const uint32_t m_srcLen;
+    const void*    m_src    = nullptr;
+    const uint32_t m_srcLen = 0;
 
-    void*    m_dst;
-    uint32_t m_dstLen;
-    uint32_t m_dstFinalLen;
+    void*    m_dst         = nullptr;
+    uint32_t m_dstLen      = 0;
+    uint32_t m_dstFinalLen = 0;
 
-    z_stream m_zstrm;
-    int      m_zret;
+    z_stream m_zstrm = {};
+    int      m_zret  = 0;
 
     std::mutex m_mtx;
   };
@@ -99,12 +128,9 @@ private:
           qi->decompressionStep();
         } break;
 
+        case Item::Status::ERROR:
         case Item::Status::DONE: {
-          qi->finishDecompression();
-        } break;
-
-        case Item::Status::ERROR: {
-          qi->finishDecompression();
+          qi->ready();
         } break;
       }
     }
@@ -229,6 +255,8 @@ EXPORT SYSV_ABI int32_t sceZlibFinalize() {
 
 EXPORT SYSV_ABI int32_t sceZlibInflate(const void* src, uint32_t srcLen, void* dst, uint32_t dstLen, uint64_t* reqId) {
   if (!accessZlibQueue().inited()) return Err::Zlib::NOT_INITIALIZED;
+  if (src == nullptr || dst == nullptr || reqId == nullptr) return Err::Zlib::INVALID;
+  if (srcLen == 0 || dstLen == 0) return Err::Zlib::NOSPACE;
   LOG_USE_MODULE(libSceZlib);
   LOG_TRACE(L"Inflate request: %p[%u], %p[%u] -> %p", src, srcLen, dst, dstLen, reqId);
   *reqId = accessZlibQueue().addTask(src, srcLen, dst, dstLen);
@@ -237,6 +265,7 @@ EXPORT SYSV_ABI int32_t sceZlibInflate(const void* src, uint32_t srcLen, void* d
 
 EXPORT SYSV_ABI int32_t sceZlibWaitForDone(uint64_t* reqId, uint32_t* timeout) {
   if (!accessZlibQueue().inited()) return Err::Zlib::NOT_INITIALIZED;
+  if (timeout == nullptr || reqId == nullptr) return Err::Zlib::INVALID;
   LOG_USE_MODULE(libSceZlib);
   LOG_TRACE(L"Waiting for queue item: %p, %p", reqId, timeout);
   return accessZlibQueue().waitForDone(reqId, timeout) ? Ok : Err::Zlib::TIMEDOUT;
@@ -244,6 +273,7 @@ EXPORT SYSV_ABI int32_t sceZlibWaitForDone(uint64_t* reqId, uint32_t* timeout) {
 
 EXPORT SYSV_ABI int32_t sceZlibGetResult(uint64_t reqId, uint32_t* dstLen, int* status) {
   if (!accessZlibQueue().inited()) return Err::Zlib::NOT_INITIALIZED;
+  if (dstLen == nullptr || status == nullptr) return Err::Zlib::INVALID;
   LOG_USE_MODULE(libSceZlib);
   LOG_TRACE(L"Get result for: %p, %p, %p", reqId, dstLen, status);
   return accessZlibQueue().getResult(reqId, dstLen, status);
